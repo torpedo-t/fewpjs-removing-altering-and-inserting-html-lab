0ca5bd8a0f69fda63b3351b39e6b33a3873303cf
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-10-20 15:15:23 -0500

    commit tree 591deb058a6fe62575849cb4bf67d4ea50ce14a8

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Write your code here!
 // remove 'main#main', expect <main id="main"></main to not exist
 // id selector #
-var main = document.getElementById('main')
+let main = document.getElementById('main')
 main.remove()
 
-var newHeader = document.createElement("h1")
+let newHeader = document.createElement("h1")
 newHeader.id = "victory"
+debugger
 newHeader.innerHTML = "YOUR-NAME is the champion"
 document.body.appendChild(newHeader)
 // document.body gives us access to the body
